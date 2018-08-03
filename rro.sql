8183a37ea9d887e825f02ed6e9a31034ca19f6af
Roman <rro@bk.ru>
2018-08-03 16:06:08 +0300

    add data to table students and cities

--- rro.sql
+++ rro.sql
@@ -19,3 +19,26 @@ CREATE TABLE Students (
     PRIMARY KEY (StudentID),
     FOREIGN KEY (CityID) REFERENCES Cities(CityID)
 );
+
+INSERT INTO Cities (City, Country)
+VALUES
+('Erfurt', 'Germany'),
+('San-Francisco', 'USA'),
+('Kapetown', 'Republic of South Africa '),
+('Beijing', 'China'),
+('Essen', 'Germany'),
+('Hamburg', 'Germany'),
+('Atlanta', 'USA');
+
+INSERT INTO Students (FirstName, LastName, NumberOfCourse, Gender, CityID)
+VALUES
+('Mark', 'Schmidt', '3', 'male', '1'),
+('Helen', 'Hunt', '2', 'male', '2'),
+('Matumba', 'Zuko', '4', 'male', '3'),
+('Rin', 'Kupo', '4', 'female', '3'),
+('Zhen', 'Chi Bao', '2', 'male', '4'),
+('Peter', 'Zimmer', '3', 'male', '5'),
+('Hanz', 'Mueller', '4', 'male', '6'),
+('Alisa', 'Kepler', '4', 'female', '1'),
+('Anna', 'Madavie', '2', 'female', '7')
+;
